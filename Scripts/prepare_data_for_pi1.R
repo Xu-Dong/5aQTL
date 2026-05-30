f8b148e4124deb59f9e01ee81026116287bb23e9
Xudong Zou <zou.xudong@foxmail.com>
2026-05-30 16:48:18 +0800

    Update prepare_data_for_pi1.R

--- Scripts/prepare_data_for_pi1.R
+++ Scripts/prepare_data_for_pi1.R
@@ -1,1 +1,18 @@
+library(data.table)
+library(dplyr)
 
+setwd("./worksplace/")
+
+qtl_file_list <- list.files("./input",pattern=".cis_5aQTL.FDR05.lead.txt")
+
+for(i in 1:length(qtl_file_list)){
+        qtl <- qtl_file_list[i]
+        tissue <- strsplit(qtl,split=".",fixed=T)[[1]][1]
+        qtl_dat <- fread(paste0("input/",qtl),header=T,sep="\t")
+        names(qtl_dat)<-c("id","pvalue")
+        leadqtl <- merge(leadqtl,qtl_dat,by="id",all.x=TRUE)
+        names(leadqtl)[i+1] <- tissue
+}
+
+
+saveRDS(leadqtl,"./output/cis_atQTL.FDR05.lead.merge.rds")
